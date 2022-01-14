a0217af9ee300b43140307a6db6c357c0961adb0
ddderi <maxime830@live.fr>
2022-01-14 05:00:18 +0100

    commit tree 7e433bf0c9de1d7f2c98dadae9e153b9c23afd43

--- index.js
+++ index.js
@@ -1,16 +1,9 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
 
 
-// console.log(batteryBatches.reduce((totalBatteries, element) => 
-//      element + totalBatteries
-//  , 0))
-
-let totalBatteries = batteryBatches.reduce((partialResult, element) => partialResult + element,0)
+let totalBatteries = batteryBatches.reduce((accum, elem) => {
+    return accum + elem
+}, 0)
 
 
-// function accum(array){
-//     let totalBatteries = 0;
-//     return array.reduce(n => n + totalBatteries)
-// }
